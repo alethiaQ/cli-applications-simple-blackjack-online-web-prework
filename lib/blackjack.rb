0acf6480a7d9482c925ad6faa1f3b794000f6a15
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 20:13:13 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   return rand(12)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card)
+  puts "Your cards add up to #{card}"
 end
 
 def prompt_user
