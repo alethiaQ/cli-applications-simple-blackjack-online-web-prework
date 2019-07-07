8d37044fe0276b761d360bef90f2cc60cb34ff05
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 20:37:40 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,6 +57,15 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  prompt_user
+  hit?
+  input = get_user_input
+  case input
+  when "s"
+    hit?
+
+  
 end
     
