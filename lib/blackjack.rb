fe99ea0b86540cde0eb8afe0e7eaa2496a4d6443
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 20:18:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -24,7 +24,10 @@ def end_game(card_tot)
 end
 
 def initial_round
-  # code #initial_round here
+ card_one = deal_card
+ card_two = deal_card
+ card_total = card_one + card_two 
+ display_card_total(card_total)
 end
 
 def hit?
