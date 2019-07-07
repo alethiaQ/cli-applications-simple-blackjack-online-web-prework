7dd0e6d6f6c1dc552d21111f5bc77f5ac6c199c2
Alethia Quintero <alethiaq22@gmail.com>
2019-07-07 20:55:31 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,7 +62,7 @@ def runner
   card_total = initial_round
     until card_total > 21 do 
     
-     card_total = hit?
+     card_total = hit?(card_total)
     input = get_user_input
     if input == "s"
       hit?
